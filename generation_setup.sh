ab58e457328d990ba672333b8b3d7f1fed1f4556
cotton-ahn <cotton.ahn@gmail.com>
2022-03-22 19:33:35 +0100

    auxilirary

---
+++ generation_setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp generation/*.py clevr-dataset-gen/image_generation
+cp generation/*.blend clevr-dataset-gen/image_generation/data
+cp generation/*.json clevr-dataset-gen/image_generation/data
